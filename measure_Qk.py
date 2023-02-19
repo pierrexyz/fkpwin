554170ad794fbb602abb329dada422c31affd783
pierrexyz <pierrexyz@gmail.com>
2023-02-20 00:09:53 +0100

    normalization accuracy improved, added integral constraint and wide-angle effects

--- measure_Qk.py
+++ measure_Qk.py
@@ -2,14 +2,14 @@ from nbodykit.lab import *
 from nbodykit import setup_logging, style
 import sys, os
 import numpy as np
-
-# catalog_dir = 'catalogs' 
-catalog_dir = os.path.join('/', 'exports', 'pierre', 'lssmap', 'catalog')
+from numpy.linalg import norm
 
 # Read command line arguments
-red = str(sys.argv[1]) # cmass or lowz
-sky = str(sys.argv[2]) # ngc or sgc
-boxside = float(sys.argv[3]) # 100000, 30000, 3500, 1000
+Nmesh = 2**10 # 2^10 = 1024: this might require a lot of memory, (2^10)^3 * 8 bits ~ 8.5Gb!
+n_wide = int(sys.argv[1]) # wide-angle expansion order
+red = str(sys.argv[2]) # cmass or lowz
+sky = str(sys.argv[3]) # ngc or sgc
+boxside = float(sys.argv[4]) # 100000, 30000, 3500, 1000
 boxsize = [boxside, boxside, boxside]
 
 if 'lowz' in red:
@@ -23,6 +23,10 @@ if 'ngc' in sky:
     skybin = 'North'
 elif 'sgc' in sky:
     skybin = 'South'
+
+# adjust paths
+catalog_dir = os.path.join('/', 'cluster', 'work', 'senatore', 'lssdata')
+out_dir = os.path.join('/', 'cluster', 'work', 'senatore', 'fkpwin', 'Qk_Lbox%.0f') % boxside
 
 # NOTE: change this path if you downloaded the data somewhere else!
 data_path = os.path.join(catalog_dir, 'galaxy_DR12v5_CMASSLOWZTOT_%s.fits') % (skybin)
@@ -51,8 +55,11 @@ data['Position'] = transform.SkyToCartesian(data['RA'], data['DEC'], data['Z'], 
 randoms['Position'] = transform.SkyToCartesian(randoms['RA'], randoms['DEC'], randoms['Z'], cosmo=cosmo)
 
 # add completeness weight: no systematic weights for randoms by construction
-randoms['WEIGHT'] = 1.0
 data['WEIGHT'] = data['WEIGHT_SYSTOT'] * (data['WEIGHT_NOZ'] + data['WEIGHT_CP'] - 1.0)
+randoms['WEIGHT'] = 1.0
+
+if n_wide > 0: # wide-angle expansion of order n: add a factor r_2^{-n} in the integral over d^3r_2 r_2^{-n} e^{-i k r_2} F(r_2) L_\ell(\hat k \cdot \hat r_2) 
+    randoms['WEIGHT_FKP_2'] = randoms['WEIGHT_FKP'] * norm(randoms['Position'].compute(), axis=-1)**-n_wide 
 
 print ('coordinates transformed to distances')
 
@@ -61,14 +68,20 @@ fkp = FKPCatalog(randoms, None, BoxPad=0.) #, randoms)
 
 print ('FKP catalogs instanciated')
 
-mesh = fkp.to_mesh(Nmesh=512, BoxSize=boxsize, nbar='NZ', 
-    fkp_weight='WEIGHT_FKP', comp_weight='WEIGHT', 
-                   resampler='pcs', interlaced=True) 
+# dtype='c16' or 'c8' to get correct odd multipoles, see https://nbodykit.readthedocs.io/en/latest/api/_autosummary/nbodykit.algorithms.convpower.catalog.html#nbodykit.algorithms.convpower.catalog.FKPCatalog.to_mesh
+mesh = fkp.to_mesh(Nmesh=Nmesh, BoxSize=boxsize, nbar='NZ', dtype='c8', # 'c8' for lower memory usage
+    fkp_weight='WEIGHT_FKP', comp_weight='WEIGHT', resampler='tsc', interlaced=True) 
+
+if n_wide == 0: 
+    mesh2 = None
+elif n_wide > 0: 
+    mesh2 = fkp.to_mesh(Nmesh=Nmesh, BoxSize=boxsize, nbar='NZ', dtype='c8', 
+        fkp_weight='WEIGHT_FKP_2', comp_weight='WEIGHT', resampler='tsc', interlaced=True) 
 
 print ('mesh grid created')
 
 # compute the multipoles
-r = ConvolvedFFTPower(mesh, poles=[0,2,4], kmin=0.)
+r = ConvolvedFFTPower(mesh, poles=[0,1,2,3,4], second=mesh2, kmin=0.)
 
 print ('multipole computed')
 
@@ -76,13 +89,12 @@ print ('multipole computed')
 alpha = 1.0 * data.csize / randoms.csize
 
 # window
-win = np.stack([r.poles['k'], r.poles['power_0'].real, r.poles['power_2'].real, r.poles['power_4'].real]).T
+win = np.stack([r.poles['k'], r.poles['power_0'].real, r.poles['power_1'].imag, r.poles['power_2'].real, r.poles['power_3'].imag, r.poles['power_4'].real]).T
 
-# np.savetxt(os.path.join('window_boss_%s_%s.dat') % (red, sky), 
-np.savetxt(os.path.join('/', 'exports', 'pierre', 'lssmap', 'spec', 'window_boss_%s_%s.dat') % (red, sky), 
-    win, fmt='%.6e', header='k, w0, w2, w4, norm, alpha = %s, %s' % (r.attrs["data.norm"], alpha))
-
-# r.save(os.path.join('/', 'exports', 'pierre', 'lssmap', 'spec', "window_boss_%s_%s.json" % (red, sky)))
+# saving file
+if not os.path.exists(out_dir): os.makedirs(out_dir)
+np.savetxt(os.path.join(out_dir, 'Qk_n%s_boss_%s_%s.dat') % (n_wide, red, sky), 
+    win, fmt='%.6e', header='k, q0, q1/i, q2, q3/i, q4, norm, alpha = %s, %s' % (r.attrs["data.norm"], alpha))
 
 print ('file saved')
 
