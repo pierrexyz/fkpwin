88507afcdc852edfa410629462771aa363b9b88c
pierrexyz <pierrexyz@gmail.com>
2023-02-21 14:39:39 +0100

    nseries

--- measure_Qk_nseries.py
+++ measure_Qk_nseries.py
@@ -3,41 +3,32 @@ from nbodykit import setup_logging, style
 import sys, os
 import numpy as np
 from numpy.linalg import norm
+from scipy.interpolate import interp1d
 
 # Read command line arguments
 Nmesh = 2**10 # 2^10 = 1024: this might require a lot of memory, (2^10)^3 * 6 * 16 bits ~ 100Gb! 
 n_wide = int(sys.argv[1]) # wide-angle expansion order
-red = str(sys.argv[2]) # cmass or lowz
-sky = str(sys.argv[3]) # ngc or sgc
-boxside = float(sys.argv[4]) # 100000, 35000, 10000, 3500
+boxside = float(sys.argv[2]) # 100000, 35000, 10000, 3500
 boxsize = [boxside, boxside, boxside]
 
-if 'lowz' in red:
-    ZMIN = 0.2
-    ZMAX = 0.43
-elif 'cmass' in red or 'nseries' in red:
-    ZMIN = 0.43
-    ZMAX = 0.7
-
-if 'ngc' in sky:
-    skybin = 'North'
-elif 'sgc' in sky:
-    skybin = 'South'
+ZMIN = 0.43
+ZMAX = 0.7
 
 # adjust paths
-catalog_dir = os.path.join('/', 'cluster', 'work', 'senatore', 'lssdata')
+catalog_dir = os.path.join('/', 'cluster', 'work', 'senatore', 'lssdata', 'nseries')
 out_dir = os.path.join('/', 'cluster', 'work', 'senatore', 'fkpwin', 'Qk_', 'Qk_Lbox%.0f') % boxside
 
 # NOTE: change this path if you downloaded the data somewhere else!
-data_path = os.path.join(catalog_dir, 'galaxy_DR12v5_CMASSLOWZTOT_%s.fits') % (skybin)
-randoms_path = os.path.join(catalog_dir, 'random0_DR12v5_CMASSLOWZTOT_%s.fits') % (skybin)
-
+data_path = os.path.join(catalog_dir, 'CutskyN1.rdzw') # I put box 1 here
+randoms_path = os.path.join(catalog_dir, 'Nseries_cutsky_randoms_50x_redshifts.dat')
 print (data_path)
 print (randoms_path)
 
-# initialize the FITS catalog objects for data and randoms
-data = FITSCatalog(data_path)
-randoms = FITSCatalog(randoms_path)
+# initialize the catalog objects for data and randoms
+data_names = ['RA', 'DEC', 'Z', 'WEIGHT_FKP', 'WEIGHT']
+data = CSVCatalog(data_path, data_names)
+randoms_names = ['RA', 'DEC', 'Z', 'WEIGHT_FKP', 'WEIGHT']
+randoms = CSVCatalog(randoms_path, randoms_names)
 
 # slice the data
 valid = (data['Z'] > ZMIN)&(data['Z'] < ZMAX)
@@ -54,9 +45,10 @@ cosmo = cosmology.Cosmology(h=0.676).match(Omega0_m=0.31)
 data['Position'] = transform.SkyToCartesian(data['RA'], data['DEC'], data['Z'], cosmo=cosmo)
 randoms['Position'] = transform.SkyToCartesian(randoms['RA'], randoms['DEC'], randoms['Z'], cosmo=cosmo)
 
-# add completeness weight: no systematic weights for randoms by construction
-data['WEIGHT'] = data['WEIGHT_SYSTOT'] * (data['WEIGHT_NOZ'] + data['WEIGHT_CP'] - 1.0)
-randoms['WEIGHT'] = 1.0
+# add density from file
+zz, nz = np.loadtxt(os.path.join(catalog_dir, 'nbar_DR12v5_CMASS_North_om0p31_Pfkp10000.dat'), usecols=(0,3), unpack=True)
+data['NZ'] = interp1d(zz, nz, kind='cubic')(data['Z'])
+randoms['NZ'] = interp1d(zz, nz, kind='cubic')(randoms['Z'])
 
 if n_wide > 0: # wide-angle expansion of order n: add a factor r_2^{-n} in the integral over d^3r_2 r_2^{-n} e^{-i k r_2} F(r_2) L_\ell(\hat k \cdot \hat r_2) 
     randoms['WEIGHT_FKP_2'] = randoms['WEIGHT_FKP'] * norm(randoms['Position'].compute(), axis=-1)**-n_wide 
@@ -93,7 +85,7 @@ win = np.stack([r.poles['k'], r.poles['power_0'].real, r.poles['power_1'].imag, 
 
 # saving file
 if not os.path.exists(out_dir): os.makedirs(out_dir)
-np.savetxt(os.path.join(out_dir, 'Qk_n%s_boss_%s_%s.dat') % (n_wide, red, sky), 
+np.savetxt(os.path.join(out_dir, 'Qk_n%s_nseries.dat') % (n_wide), 
     win, fmt='%.6e', header='k, q0, q1/i, q2, q3/i, q4, norm, alpha = %s, %s' % (r.attrs["data.norm"], alpha))
 
 print ('file saved')
